d0daa66d4c6959bb0b2ef52781dc375aea701d0d
laurelea <laurelea@mail.ru>
2021-09-06 20:59:36 +0500

    second commit

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-updater = Updater(token='1934217722:AAEmDIq4bZkN1lJg1d2pcUvBD1K1t76T8z8', use_context=True)
-dispatcher = updater.dispatcher
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, ForceReply
+import logging
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+logger = logging.getLogger("WB Bot Logger")
+
+# def start(update, context):
+#     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+#     print('test it!')
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
 
 def inline_caps(update, context):
     query = update.inline_query.query
@@ -36,16 +41,47 @@ def inline_caps(update, context):
     )
     context.bot.answer_inline_query(update.inline_query.id, results)
 
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
-
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+def brute_force_stop(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot now stops")
+    try:
+        updater.stop()
+    except Exception as error:
+        logger.error(error)
 
-updater.start_polling()
+
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
 # updater.stop()
 # updater.idle()
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token='1934217722:AAEmDIq4bZkN1lJg1d2pcUvBD1K1t76T8z8', use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("stop", brute_force_stop))
+    dispatcher.add_handler(InlineQueryHandler(inline_caps))
+    dispatcher.add_handler(CommandHandler('caps', caps))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+    # Start the Bot
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
