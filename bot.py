b32da69a55a35fe440844b3947688b7a28836639
p.v.sukhorukova <p.v.sukhorukova@tmn3.etagi.com>
2021-09-10 12:21:43 +0500

    Update main branch before adding ConversationHandler

--- bot.py
+++ bot.py
@@ -1,87 +1,235 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext
-from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext, ConversationHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger("WB Bot Logger")
+from parser import Parser
 
 # def start(update, context):
 #     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 #     print('test it!')
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+class Bot:
+    def __init__(self):
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
         )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+        self.logger = logging.getLogger("WB Bot Logger")
 
-def brute_force_stop(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot now stops")
-    try:
-        updater.stop()
-    except Exception as error:
-        logger.error(error)
+        # Create the Updater and pass it your bot's token.
+        self.updater = Updater(token='1934217722:AAEmDIq4bZkN1lJg1d2pcUvBD1K1t76T8z8', use_context=True)
+
+        # Get the dispatcher to register handlers
+        self.dispatcher = self.updater.dispatcher
+
+        # self.chat_data = []
+
+    def start(self, update: Update, context: CallbackContext) -> None:
+        """Send a message when the command /start is issued."""
+        user = update.effective_user
+        update.message.reply_markdown_v2(
+            fr'Hi {user.mention_markdown_v2()}\!',
+                              reply_markup=self.markup
+        )
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Type /help for possible commands')
+
+    def add_suggested_actions(self, update, context):
+        options = []
+
+        options.append(InlineKeyboardButton('Enter Name', callback_data='name'))
+        options.append(InlineKeyboardButton('Enter Age', callback_data='age'))
+
+        reply_markup = InlineKeyboardMarkup([options])
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Choose an option', reply_markup=reply_markup)
+    # Что-то такое нужно добавить для обработки ответов: updater.dispatcher.add_handler(CallbackQueryHandler(add_suggested_actions, pattern='m*'))
+
+    def test(self, update, context):
+        update.message.reply_text('Type smth')
+
+    def error(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Error occured, see logs for details')
+        print(f'Update {update.message["text"]} caused error: {context.error}')
+
+    def location(self, update: Update, context: CallbackContext) -> int:
+        """Stores the location and asks for some info about the user."""
+        user = update.message.from_user
+        user_location = update.message.location
+        self.logger.info(
+            "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+        )
+        update.message.reply_text(
+            'Maybe I can visit you sometime! At last, tell me something about yourself.'
+        )
+
+        return BIO
+
+    def echo(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    def caps(self, update, context):
+        text_caps = ' '.join(context.args).upper()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+    def set_discount(self, update, context):
+        self.current_discount = int(''.join(context.args))
+        print(self.current_discount)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Current discount now is {self.current_discount}')
+
+    def unknown(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+    def brute_force_stop(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Bot now stops")
+        try:
+            self.updater.stop()
+        except Exception as error:
+            self.logger.error(f'brute_froce_stop error: {error}')
+
+    def help_command(self, update: Update, context: CallbackContext) -> None:
+        """Send a message when the command /help is issued."""
+        update.message.reply_text(''
+                                  '/parser - Start parser\n'
+                                  '/update - Run command "update_prices" after parser is started \n'
+                                  '/show - Run command "show_items" after parser is started \n'
+                                  '/stop - Brute-force stop \n'
+                                  '/close - Close keyboard \n'
+                                  '/super discount - Create parser and run update_prices with provided discount\n'
+                                  )
+
+    def initiate_start_parser(self, update, context):
+
+        # self.current_discount = int(''.join(context.args))
+        # print('Set current discount:', self.current_discount)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Please, enter current WB discount')
+
+        # context.bot.send_message(chat_id=update.effective_chat.id, text=f'Current discount now is {self.current_discount}')
 
 
+    def start_parser(self, update, context):
+        try:
+            self.current_discount = int(update.message.text)
+            self.parser = Parser(self.current_discount)
+        except Exception as error:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text='Error occured, see logs for details')
+            print(error)
+            # update.message.reply_text('Error occured, see logs for details')
+        else:
+            update.message.reply_text(f'Parser started with current WB discount {self.current_discount}. \nType /update to update prices, /show to show currenty')
 
+    def update_prices(self, update, context):
+        if not self.parser:
+            raise Exception
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Updating of prices initiated ok')
+            try:
+                self.parser.update_prices()
+            except Exception as error:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text='Error occured in command /update, see logs for details')
+                print(error)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f'Prices updated  ok')
+    def show_items(self, update, context):
+        if not self.parser:
+            raise Exception
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Showing of items initiated ok')
+            try:
+                items = self.parser.get_db_data()
+                items_to_bot = items.loc[0:, ['articul', 'my_title', 'min_my_price', 'url']].to_markdown(index=False)
+                print(items_to_bot)
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=items_to_bot)
+            except Exception as error:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text='Error occured in command /show, see logs for details')
+                print(error)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f'Items shown  ok')
+    def super(self, update, context):
+        self.current_discount = int(''.join(context.args))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Super function initiated ok with current discount of {self.current_discount}')
+        try:
+            self.parser = Parser(self.current_discount)
+            self.parser.update_prices()
+        except:
+            print('Trying to log "except"')
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Super function finished ok')
+    def close_keyboard(self, update, context):
+        update.message.reply_text('Ok', reply_markup=ReplyKeyboardRemove())
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    def task(self, context):
+        context.bot.send_message(context.job.context, text='Хорош сидеть за компьютером, разомнись!')
 
-# updater.stop()
+    def set_timer(self, update, context):
+
+        # создаём задачу task в очереди job_queue
+        # передаём ей идентификатор текущего чата
+        # идентификатор далее будет доступен через job.context
+
+        delay = 5  # количество секунд
+        job = context.job_queue.run_once(self.task, delay, context=update.message.chat_id)
+
+        # Регистрируем созданную задачу в пользовательских данных
+        context.chat_data['job'] = job
+
+        # Сообщаем о том, что таймер установлен
+        update.message.reply_text('Таймер установлен на ' + str(delay) + ' секунд')
+
+    def unset_timer(self, update, context):
+        # Проверяем, что задача действительно установлена
+        if 'job' in context.chat_data:
+            # планируем удаление задачи
+            context.chat_data['job'].schedule_removal()
+            # и очищаем пользовательские данные
+            del context.chat_data['job']
+
+        update.message.reply_text('Таймер отменён!')
 # updater.idle()
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(token='1934217722:AAEmDIq4bZkN1lJg1d2pcUvBD1K1t76T8z8', use_context=True)
+    def main(self) -> None:
+        """Start the bot."""
+        self.reply_keyboard = [['/stop', '/start', '/help', '/close', '/set_timer', '/unset_timer', '/super', '/parser', '/update', '/show']]
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+        self.markup = ReplyKeyboardMarkup(self.reply_keyboard, one_time_keyboard=False)
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("stop", brute_force_stop))
-    dispatcher.add_handler(InlineQueryHandler(inline_caps))
-    dispatcher.add_handler(CommandHandler('caps', caps))
+        # on different commands - answer in Telegram
+        self.dispatcher.add_handler(CommandHandler("start", self.start))
+        self.dispatcher.add_handler(CommandHandler("add", self.add_suggested_actions))
+        self.dispatcher.add_handler(CommandHandler("help", self.help_command))
+        self.dispatcher.add_handler(CommandHandler("stop", self.brute_force_stop))
+        # self.dispatcher.add_handler(CommandHandler("start_again", self.brute_force_start))
+        self.dispatcher.add_handler(CommandHandler('caps', self.caps))
+        self.dispatcher.add_handler(CommandHandler('discount', self.set_discount))
+        self.dispatcher.add_handler(CommandHandler("test", self.test))
+        self.dispatcher.add_handler(CommandHandler("parser", self.initiate_start_parser))
+        self.dispatcher.add_handler(CommandHandler("update", self.update_prices))
+        self.dispatcher.add_handler(CommandHandler("show", self.show_items))
+        self.dispatcher.add_handler(CommandHandler("super", self.super))
+        self.dispatcher.add_handler(CommandHandler("close", self.close_keyboard))
+        self.dispatcher.add_handler(CommandHandler("set_timer", self.set_timer, pass_job_queue=True, pass_chat_data=True))
+        self.dispatcher.add_handler(CommandHandler("unset_timer", self.unset_timer, pass_chat_data=True))
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+        # on non command i.e message - echo the message on Telegram
+        # self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.echo))
+        self.dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
+        self.dispatcher.add_handler(MessageHandler(Filters.location, self.location))
+        self.dispatcher.add_handler(MessageHandler(Filters.text, self.start_parser))
 
-    # Start the Bot
-    updater.start_polling()
+        self.dispatcher.add_error_handler(self.error)
+
+        # Start the Bot
+        self.updater.start_polling()
+
+        self.updater.idle()
+
+
+
 
 if __name__ == '__main__':
-    main()
+    bot = Bot()
+    bot.main()
--- parser.py
+++ parser.py
@@ -4,22 +4,14 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
+import pandas as pd
+import lxml
 
-connection = psycopg2.connect(
-    host='ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
-    port='5432',
-    user='gsoxtcmgsqbxbi',
-    password='42bd9db46f114e1424c2180635a13daf02b6e08cc8383dcf2ba8973748870b85',
-    database='dbfo3d84h3ca49')
-
-cursor = connection.cursor()
-
-
-itemsToMonitor = {
-    'Opletka light pink': ['https://www.wildberries.ru/catalog/8618192/detail.aspx?size=29100958', 5],
-    'Opletka bright pink': ['https://www.wildberries.ru/catalog/8618199/detail.aspx?size=29100965', 5],
-    'Depil lotion': ['https://www.wildberries.ru/catalog/21370282/detail.aspx?size=54366342', 2],
-    'Ghi': ['https://www.wildberries.ru/catalog/12650422/detail.aspx?size=39068569', 2],
+itemsToAdd = {
+    # 'Opletka light pink': ['https://www.wildberries.ru/catalog/8618192/detail.aspx?size=29100958', 5],
+    # 'Opletka bright pink': ['https://www.wildberries.ru/catalog/8618199/detail.aspx?size=29100965', 5],
+    # 'Depil lotion': ['https://www.wildberries.ru/catalog/21370282/detail.aspx?size=54366342', 2],
+    # 'Ghi': ['https://www.wildberries.ru/catalog/12650422/detail.aspx?size=39068569', 2],
     # 'Crocs Black': ['https://www.wildberries.ru/catalog/17970897/detail.aspx?size=49259048'],
     # 'Crocs lilac': ['https://www.wildberries.ru/catalog/18814253/detail.aspx?size=50705766'],
     # 'Bandana fuchsia': ['https://www.wildberries.ru/catalog/28103090/detail.aspx?size=63768471', 2],
@@ -29,44 +21,116 @@ itemsToMonitor = {
     # 'Dark striped skirt': ['https://www.wildberries.ru/catalog/12261911/detail.aspx?size=38213553', 7]
 }
 
-#Читать из ДБ старые данные по мин цене
+class Parser:
+    def __init__(self, current_discount=12):
+        pd.set_option('display.max_rows', 1000)
+        pd.set_option('display.max_columns', 1000)
+        pd.set_option("display.colheader_justify", "center")
+        pd.set_option('display.width', 600)
+        pd.set_option('max_colwidth', 30)
+        pd.set_option('display.expand_frame_repr', False)
 
-current_discount = 14
+        self.connection = psycopg2.connect(
+            host='ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
+            port='5432',
+            user='gsoxtcmgsqbxbi',
+            password='42bd9db46f114e1424c2180635a13daf02b6e08cc8383dcf2ba8973748870b85',
+            database='dbfo3d84h3ca49')
 
-for key, value in itemsToMonitor.items():
-    url = value[0]
-    maxDiscount = value[1] if len(value) > 1 else None
-    name = key
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    price = soup.find('span', class_='price-block__final-price').text.strip().replace(' ₽', '').replace(' ', '')
-    print(f'Price: {price}')
-    wbTitle = ' / '.join([i.text for i in soup.find('h1', class_='same-part-kt__header').find_all('span')])[:20]
-    print(f'wbTitle: {wbTitle}')
+        self.current_discount = current_discount
 
-    articul = soup.find('span', {'data-link': "text{: selectedNomenclature^cod1S}"}).text
-    # for elem in wbTitle:
-    #     print('elem: ', elem)
-    print(price, wbTitle, articul)
-    if maxDiscount:
-        minMyPrice = int(price) - int(price) * int(maxDiscount) / 100
-    else:
-        minMyPrice = int(price) - int(price) * int(current_discount) / 100
-    #Добавить условие для мин цены, поменять на UPDATE
-    query = """ INSERT INTO wb (articul, wb_title, my_title, min_my_price, min_price_date, url, min_base_price, my_discount, max_discount) 
-    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    ON CONFLICT (articul) DO UPDATE
-      SET articul = excluded.articul, wb_title = excluded.wb_title, min_my_price = excluded.min_my_price
-    """
-    values = (articul, wbTitle, name, minMyPrice, datetime.now().date(), url, price, current_discount, maxDiscount)
 
-    try:
-        cursor.execute(query, values)
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record inserted successfully into mobile table")
-    except Exception as error:
-        traceback.print_exc()
+    def get_db_data(self):
+        try:
+            itemsFromDB_df = pd.read_sql_query('''select * from wb''', self.connection)
+        except Exception:
+            traceback.print_exc()
+        else:
+            return itemsFromDB_df
 
-cursor.close()
-connection.close()
+    def insert_new(self, current_discount):
+        cursor = self.connection.cursor()
+        for key, value in itemsToAdd.items():
+            url = value[0]
+            maxDiscount = value[1] if len(value) > 1 else None
+            name = key
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            price = soup.find('span', class_='price-block__final-price').text.strip().replace(' ₽', '').replace(' ', '')
+            print(f'Price: {price}')
+            wbTitle = ' / '.join([i.text for i in soup.find('h1', class_='same-part-kt__header').find_all('span')])[:20]
+            print(f'wbTitle: {wbTitle}')
+
+            articul = soup.find('span', {'data-link': "text{: selectedNomenclature^cod1S}"}).text
+            print(price, wbTitle, articul)
+            if maxDiscount:
+                minMyPrice = int(price) - int(price) * int(maxDiscount) / 100
+            else:
+                minMyPrice = int(price) - int(price) * int(current_discount) / 100
+            query = """ INSERT INTO wb (articul, wb_title, my_title, min_my_price, min_price_date, url, min_base_price, my_discount, max_discount) 
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            ON CONFLICT (articul) DO UPDATE
+              SET articul = excluded.articul, wb_title = excluded.wb_title, min_my_price = excluded.min_my_price
+            """
+            values = (articul, wbTitle, name, minMyPrice, datetime.now().date(), url, price, current_discount, maxDiscount)
+
+            try:
+                cursor.execute(query, values)
+                self.connection.commit()
+            except Exception:
+                traceback.print_exc()
+        cursor.close()
+
+    # insert_new(current_discount)
+
+    def update_prices(self):
+        current_discount = self.current_discount
+        myDB = self.get_db_data()
+        cursor = self.connection.cursor()
+        for date, row in myDB.T.iteritems():
+            url = row['url']
+            min_my_price = row['min_my_price']
+            max_discount = row['max_discount']
+            item_id = row['item_id']
+            my_title = row['my_title']
+            min_base_price = row['min_base_price']
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            base_price = int(soup.find('span', class_='price-block__final-price').text.strip().replace(' ₽', '').replace(' ', ''))
+            if base_price == None:
+                continue
+            if pd.isna(max_discount):
+                current_min_price = round(base_price - (base_price * current_discount) / 100)
+            else:
+                current_min_price = round(base_price - (base_price * max_discount) / 100)
+                # Check current discount on item:
+                if current_min_price < min_my_price:
+                    current_item_discount = int(input(f"Enter current discount for item {url}:\n"))
+                    if current_item_discount != max_discount:
+                        current_min_price = round(base_price - (base_price * current_item_discount) / 100)
+            #Update:
+            if current_min_price < min_my_price or base_price < min_base_price:
+                query = '''
+                    UPDATE wb
+                    SET min_my_price = %s, my_discount = %s, min_price_date = current_date, min_base_price = %s
+                    WHERE item_id = %s
+                '''
+                cursor.execute(query, (min(current_min_price, min_my_price), current_discount, min(min_base_price, base_price), item_id))
+                self.connection.commit()
+                print(f'\nDB data changed for: {my_title}. Min price changed from {min_my_price} to {min(current_min_price, min_my_price)}, min base price changed from {min_base_price} to {min(min_base_price, base_price)}')
+        cursor.close()
+        print('update_prices finished')
+        return True
+    # update_prices(current_discount)
+    def close_connection(self):
+        self.connection.close()
+
+if __name__ == '__main__':
+
+    print('Parser запущен сам по себе')
+
+    current_discount = int((input('Enter current discount: ')))
+    parser = Parser(current_discount)
+    raise SystemExit(parser.update_prices(current_discount))
+else:
+    print('Parser импортирован в другой модуль.')
